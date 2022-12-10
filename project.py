22d213f597a7472f3993972e8d4caf7d402c110a
Renal-Tarek <104008582+Renal-Tarek@users.noreply.github.com>
2022-12-10 23:50:45 +0200

    Update project.py

--- project.py
+++ project.py
@@ -49,3 +49,14 @@ def rover_coords(binary_img):
     x_pixel = -(ypos - binary_img.shape[0]).astype(np.float32)
     y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float32)
     return x_pixel, y_pixel
+
+# Define a function to convert to radial coords in rover space
+def to_polar_coords(x_pixel, y_pixel):
+    # Convert (x_pixel, y_pixel) to (distance, angle) 
+    # in polar coordinates in rover space
+    # Calculate distance to each pixel
+    dist = np.sqrt(x_pixel**2 + y_pixel**2)
+    # Calculate angle away from vertical for each pixel
+    angles = np.arctan2(y_pixel, x_pixel)
+    return dist, angles
+
