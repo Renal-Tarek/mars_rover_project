f8eac631800e42aa7cbe8b56257c2918a3f7926d
AhmedReda3 <106000793+AhmedReda3@users.noreply.github.com>
2022-12-10 23:48:54 +0200

    Update project.py

--- project.py
+++ project.py
@@ -26,3 +26,17 @@ def color_thresh_obsticals(img, rgb_thresh=(160, 160, 160)):
                 &(img[:,:,2] != 0)                
     color_select[below_thresh] = 1
     return color_select
+
+def color_thresh_rock(img):
+    
+    img1=img[:,:,::-1]
+    hsv = cv2.cvtColor(img1, cv2.COLOR_RGB2HSV)
+
+    #Define range of yellow colors in HSV
+    lower_yellow = np.array([10, 100, 100],dtype='uint8')
+    upper_yellow = np.array([255, 225 ,255],dtype='uint8')
+    
+    #Threshold the HSV image to get only yellow colors
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    #mask=cv2.bitwise_and(img,img,mask=out)
+    return mask
