059fdfec41053513bd42c269682159be0285d711
Shorouk Hegazy <105971383+ShoroukHegazy15@users.noreply.github.com>
2022-12-10 23:48:13 +0200

    Update project.py

--- project.py
+++ project.py
@@ -14,3 +14,15 @@ def color_thresh_obsticals(img, rgb_thresh=(160, 160, 160)):
                 &(img[:,:,2] != 0)                
     color_select[below_thresh] = 1
     return color_select
+
+def color_thresh_obsticals(img, rgb_thresh=(160, 160, 160)):
+
+    color_select = np.zeros_like(img[:,:,0])
+    below_thresh = (img[:,:,0] < rgb_thresh[0]) \
+                & (img[:,:,1] < rgb_thresh[1]) \
+                & (img[:,:,2] < rgb_thresh[2]) \
+                &(img[:,:,0] != 0) \
+                &(img[:,:,1] != 0) \
+                &(img[:,:,2] != 0)                
+    color_select[below_thresh] = 1
+    return color_select
